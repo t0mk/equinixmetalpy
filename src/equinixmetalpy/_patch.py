c275d026a521e84f5c926f4cd28d66f4dfbccaa3
Tomas Karasek <tom.to.the.k@gmail.com>
2023-02-13 14:27:46 +0100

    make debug prints look like packngo

--- src/equinixmetalpy/_patch.py
+++ src/equinixmetalpy/_patch.py
@@ -20,6 +20,7 @@ import sys
 import re
 import types
 import json
+import datetime
 from http.client import responses
 
 
@@ -40,22 +41,19 @@ def prettyjson(txt):
     return json.dumps(json.loads(txt), sort_keys=True, indent=4, separators=(",", ": "))
 
 
-def get_bottom_divider(topdiv):
-    return "└" + "─" * (len(topdiv) - 2) + "┘" + "\n"
-
-
-def get_top_divider(title):
-    return "┌{}[ {} ]{}┐".format("─" * 4, title, "─" * 4)
-
-
 def get_headers_string(headers_dict):
-    log_string = "Headers:"
+    log_string = ""
     for header, value in headers_dict.items():
         if header.lower() in HEADERS_TO_SANITIZE:
-            log_string += "\n  '{}': REDACTED".format(header)
+            log_string += "{}: REDACTED\n".format(header)
         else:
-            log_string += "\n  '{}': '{}'".format(header, value)
+            log_string += "{}: {}\n".format(header, value)
     return log_string
+
+
+def get_timestamp():
+    # get timestamp in current timezone
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 class HttpLoggingPolicy(SansIOHTTPPolicy):
@@ -79,11 +77,10 @@ class HttpLoggingPolicy(SansIOHTTPPolicy):
         :param request: The PipelineRequest object.
         :type request: ~azure.core.pipeline.PipelineRequest
         """
-        topdiv = get_top_divider(
-            "Http {} to {}".format(
-                request.http_request.method,
-                request.http_request.url.replace(self.api_endpoint, ""),
-            )
+        self._logger.debug(get_timestamp())
+        topdiv = "=======[REQUEST]=============\nHTTP {} to {}".format(
+            request.http_request.method,
+            request.http_request.url.replace(self.api_endpoint, ""),
         )
         http_request = request.http_request
         if not self._logger.isEnabledFor(logging.DEBUG):
@@ -94,22 +91,22 @@ class HttpLoggingPolicy(SansIOHTTPPolicy):
             log_string = get_headers_string(http_request.headers)
             # We don't want to log the binary data of a file upload.
             if isinstance(http_request.body, types.GeneratorType):
-                log_string += "\nFile upload in body"
+                log_string += "File upload in body"
             elif isinstance(http_request.body, types.AsyncGeneratorType):
-                log_string += "\nFile upload"
+                log_string += "File upload"
             elif http_request.body:
                 if http_request.headers.get("content-type", "").startswith(
                     "application/json"
                 ):
-                    log_string += "\nBody:\n" + prettyjson(http_request.body)
+                    log_string += "Body:\n" + prettyjson(http_request.body)
                 else:
-                    log_string += "\nBody:\n{}".format(str(http_request.body))
+                    log_string += "Body: {}\b".format(str(http_request.body))
             else:
-                log_string += "\nEmpty body"
+                log_string += "<Empty body>"
             self._logger.debug(log_string)
         except Exception as err:  # pylint: disable=broad-except
             self._logger.debug("Failed to log request: %r", err)
-        self._logger.debug(get_bottom_divider(topdiv))
+        self._logger.debug("")
 
     def on_response(self, request, response):
         # type: (PipelineRequest, PipelineResponse) -> None
@@ -120,17 +117,17 @@ class HttpLoggingPolicy(SansIOHTTPPolicy):
         :type response: ~azure.core.pipeline.PipelineResponse
         """
         http_response = response.http_response
-        topdiv = get_top_divider(
-            'Http {} "{}" from {}'.format(
-                http_response.status_code,
-                responses.get(http_response.status_code, "Unknown"),
-                request.http_request.url.replace(self.api_endpoint, ""),
-            )
+        topdiv = '=======[RESPONSE]============\nHTTP {} ({}) from {}'.format(
+            http_response.status_code,
+            responses.get(http_response.status_code, "Unknown"),
+            request.http_request.url.replace(self.api_endpoint, ""),
         )
+
         try:
             if not self._logger.isEnabledFor(logging.DEBUG):
                 return
 
+            self._logger.debug(get_timestamp())
             self._logger.debug(topdiv)
             log_string = get_headers_string(http_response.headers)
             # We don't want to log binary data if the response is a file.
@@ -141,30 +138,30 @@ class HttpLoggingPolicy(SansIOHTTPPolicy):
 
             if header and pattern.match(header):
                 filename = header.partition("=")[2]
-                log_string += "\nBody contains file attachments: {}".format(
+                log_string += "Body contains file attachments: {}".format(
                     filename)
             elif http_response.headers.get("content-type", "").endswith("octet-stream"):
-                log_string += "\nBody contains binary data."
+                log_string += "Body contains binary data."
             elif http_response.headers.get("content-type", "").startswith("image"):
-                log_string += "\nBody contains image data."
+                log_string += "Body contains image data."
             elif http_response.headers.get("content-type", "").startswith(
                 "application/json"
             ):
-                log_string += "\nBody:\n" + prettyjson(http_response.text())
+                log_string += "Body:\n" + prettyjson(http_response.text())
             else:
                 if response.context.options.get("stream", False):
-                    log_string += "\nBody is streamable."
+                    log_string += "Body is streamable."
                 else:
                     txt = http_response.text()
                     if txt:
-                        log_string += "\nBody:\n{}".format(txt)
+                        log_string += "Body:\n{}".format(txt)
                     else:
-                        log_string += "\nEmpty body"
+                        log_string += "<Empty body>"
                     # log_string += "\n{}".format(http_response.text())
             self._logger.debug(log_string)
         except Exception as err:  # pylint: disable=broad-except
             self._logger.debug("Failed to log response: %s", repr(err))
-        self._logger.debug(get_bottom_divider(topdiv))
+        self._logger.debug("")
 
 
 class Client(GeneratedClient):
@@ -195,11 +192,13 @@ def patch_sdk():
 
 
 class MetalApiError(Exception):
-    error_list = []
+    isNotFoundError = False
 
     def __init__(self, error_list):
-        self.error_list = error_list
-        super().__init__(", ".join([e for e in error_list]))
+        if "Not found" in error_list:
+            self.isNotFoundError = True
+        super().__init__(" ".join([e for e in error_list]))
+
     pass
 
 
@@ -212,8 +211,10 @@ def is_error(result):
     return False
 
 
-def collect_error_list(err_result):
+def collect_error_list(err_result, prefix):
     err_list = []
+    if prefix != "":
+        err_list.append(prefix)
     if err_result.error is not None:
         err_list.append(err_result.error)
     if err_result.errors is not None:
@@ -221,7 +222,9 @@ def collect_error_list(err_result):
     return err_list
 
 
-def raise_if_error(result):
+def raise_if_error(result, prefix=""):
+    if prefix != "":
+        prefix = prefix + ":"
     if is_error(result):
-        raise MetalApiError(collect_error_list(result))
+        raise MetalApiError(collect_error_list(result, prefix))
     return result
